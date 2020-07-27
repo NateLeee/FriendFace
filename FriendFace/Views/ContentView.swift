75a0ff38269ffa6cdb56b745b5eb52c1733d3695
Nate Lee <nateleee.dev@gmail.com>
2020-07-27 18:19:59 +0800

    Go!

---
+++ FriendFace/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by Nate Lee on 7/27/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
