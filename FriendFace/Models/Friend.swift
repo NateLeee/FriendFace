c0bb7708ea3f142ce4b33305a81dfb893fb40974
Nate Lee <nateleee.dev@gmail.com>
2020-07-27 18:26:28 +0800

    Added two Model files: User & Friend.

---
+++ FriendFace/Models/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Nate Lee on 7/27/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
---
+++ FriendFace/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by Nate Lee on 7/27/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
